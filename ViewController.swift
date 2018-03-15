6f92f4c0350e0b2b6f4fffa44d27b30f0e66358d
Hyomin Jeon <hyominjeon@Hyomins-MacBook-Pro.local>
2018-03-14 22:44:38 -0700

    call yelp api to get business name

---
+++ ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by Hyomin Jeon on 03/07/18.
+//  Copyright © 2018 Hyomin Jeon. All rights reserved.
+//
+
+import UIKit
+import YelpAPI
+import Darwin
+
+class ViewController: UIViewController {
+    
+    let apiKey:String = "apiKey"
+    var label: UILabel = UILabel(frame: CGRect.init(x: 10, y: 100, width: 300, height: 20))
+    var button: UIButton = UIButton(frame: CGRect.init(x: 10, y: 140, width: 70, height: 20))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let client:YLPClient = YLPClient.init(apiKey: apiKey)
+        client.search(withLocation: "San Francisco, CA", completionHandler: {(search, error) -> Void in
+            DispatchQueue.main.async {
+                let randomIndex: Int = Int(arc4random_uniform(UInt32.init(Double((search?.businesses.count)!))))
+                self.label.text = search?.businesses[randomIndex].name
+            }
+        })
+        button.setTitle("Button", for: UIControlState.normal)
+        button.setTitleColor(UIColor.blue, for: UIControlState.normal)
+        view.addSubview(label)
+        view.addSubview(button)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
